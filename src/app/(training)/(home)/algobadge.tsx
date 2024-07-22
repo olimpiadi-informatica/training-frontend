bc8e0dda606adabbd566c554b2649e0b83199e53
Alessandro Bortolin <bortolin.alessandro@outlook.it>
2024-07-22 13:51:28 +0200

    Simplify algobadge image

--- src/app/(training)/(home)/algobadge.tsx
+++ src/app/(training)/(home)/algobadge.tsx
@@ -1,27 +1,34 @@
 import { Fragment } from "react";
 
+import { path } from "d3-path";
 import { Check, LockKeyhole, PencilLine } from "lucide-react";
 
 export function Algobadge({ className }: { className?: string }) {
-  const path = [
-    "M2,0.4",
-    "L2,0.5   Q2,0.75   1.5,0.75", // ⤶
-    "L1,0.75  Q0.5,0.75 0.5,1", // ⤹
-    "L0.5,2   Q0.5,2.25 1,2.25", // ⤷
-    "L2,2.25  Q2.5,2.25 2.5,2.5", // ⤵
-    "L2.5,3.5 Q2.5,3.75 2,3.75", // ⤶
-    "L1,3.75  Q0.5,3.75 0.5,4", // ⤹
-    "L0.5,5   Q0.5,5.25 1,5.25", // ⤷
-    "         Q2,5.25   2,5.5", // ⤵
-    "L2,5.7 M2,5.9 L2,6.1 M2,6.3 L2,6.5", // ⇣
+  const nodes = [
+    { x: 2, y: 0, icon: "check" },
+    { x: 0.5, y: 1.5, icon: "check" },
+    { x: 2.5, y: 3, icon: "pencil" },
+    { x: 0.5, y: 4.5, icon: "lock" },
+    { x: 2, y: 5.7, icon: "" },
   ];
 
-  const nodes = [
-    [2, 0, "check"],
-    [0.5, 1.5, "check"],
-    [2.5, 3, "pencil"],
-    [0.5, 4.5, "lock"],
-  ] as const;
+  const line = path();
+  line.moveTo(nodes[0].x, nodes[0].y);
+  for (let i = 1; i < nodes.length; i++) {
+    line.bezierCurveTo(
+      nodes[i - 1].x,
+      nodes[i].y + 0.1,
+      nodes[i].x,
+      nodes[i - 1].y - 0.1,
+      nodes[i].x,
+      nodes[i].y,
+    );
+  }
+  for (let i = 0; i < 2; i++) {
+    const last = nodes.at(-1)!;
+    line.moveTo(last.x, last.y + 0.15 + i * 0.35);
+    line.lineTo(last.x, last.y + 0.35 + i * 0.35);
+  }
 
   return (
     <svg
@@ -29,19 +36,26 @@ export function Algobadge({ className }: { className?: string }) {
       viewBox="0 -0.5 3 7"
       className={className}
       aria-hidden="true">
-      <path d={path.join("")} strokeWidth={0.05} className="stroke-base-content" fillOpacity={0} />
-      {nodes.map(([cx, cy, icon], i) => (
+      <path
+        d={line.toString()}
+        strokeWidth={0.05}
+        className="stroke-base-content"
+        fillOpacity={0}
+      />
+      {nodes.map(({ x, y, icon }, i) => (
         <Fragment key={i}>
-          <circle
-            cx={cx}
-            cy={cy}
-            r={0.4}
-            strokeWidth={0.05}
-            className="fill-base-200 stroke-base-content"
-          />
-          {icon === "check" && <Check x={cx - 0.25} y={cy - 0.25} size={0.5} />}
-          {icon === "pencil" && <PencilLine x={cx - 0.2} y={cy - 0.2} size={0.4} />}
-          {icon === "lock" && <LockKeyhole x={cx - 0.2} y={cy - 0.2} size={0.4} />}
+          {icon && (
+            <circle
+              cx={x}
+              cy={y}
+              r={0.4}
+              strokeWidth={0.05}
+              className="fill-base-200 stroke-base-content"
+            />
+          )}
+          {icon === "check" && <Check x={x - 0.25} y={y - 0.25} size={0.5} />}
+          {icon === "pencil" && <PencilLine x={x - 0.2} y={y - 0.2} size={0.4} />}
+          {icon === "lock" && <LockKeyhole x={x - 0.2} y={y - 0.2} size={0.4} />}
         </Fragment>
       ))}
     </svg>
