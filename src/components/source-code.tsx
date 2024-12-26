599c6d4f5a012a25bcba2090a66788456bd38bc7
Alessandro Bortolin <bortolin.alessandro@outlook.it>
2024-12-26 15:51:06 +0100

    Improve streaming

--- src/components/source-code.tsx
+++ src/components/source-code.tsx
@@ -1,3 +1,5 @@
+import { Suspense } from "react";
+
 import { Trans, msg } from "@lingui/macro";
 import { useLingui } from "@lingui/react";
 import { Code } from "@olinfo/react-components";
@@ -6,11 +8,8 @@ import { Download, FileCode2 } from "lucide-react";
 
 import { Language, fileLanguage } from "~/lib/language";
 
-export async function SourceCode({ url }: { url: string }) {
+export function SourceCode({ url }: { url: string }) {
   const { _ } = useLingui();
-
-  const resp = await fetch(url);
-  const source = await resp.text();
 
   const lang = fileLanguage(url) ?? Language.Plain;
 
@@ -26,15 +25,9 @@ export async function SourceCode({ url }: { url: string }) {
 
   return (
     <div className="relative overflow-hidden rounded-box border border-base-content/10">
-      {source !== undefined ? (
-        <Code
-          code={truncate(source, { length: 100_000 }).trimEnd()}
-          lang={lang}
-          className="max-h-[75vh] overflow-auto text-xs *:p-4"
-        />
-      ) : (
-        <div className="skeleton h-[75vh]" />
-      )}
+      <Suspense fallback={<div className="skeleton h-[75vh]" />}>
+        <SourceCodeInner url={url} lang={lang} />
+      </Suspense>
       <div className="absolute right-0 top-0 flex rounded-bl-xl border-b border-l border-base-content/10 bg-base-100">
         <a
           href={url}
@@ -45,5 +38,21 @@ export async function SourceCode({ url }: { url: string }) {
         </a>
       </div>
     </div>
+  );
+}
+
+export async function SourceCodeInner({
+  url,
+  lang,
+}: { url: string; lang: Exclude<Language, Language.Scratch> }) {
+  const resp = await fetch(url);
+  const source = await resp.text();
+
+  return (
+    <Code
+      code={truncate(source, { length: 100_000 }).trimEnd()}
+      lang={lang}
+      className="max-h-[50vh] overflow-auto text-xs *:p-4"
+    />
   );
 }
